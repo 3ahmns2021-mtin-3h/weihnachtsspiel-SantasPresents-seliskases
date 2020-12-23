64c4176306ca8d8fff78f9606e64cf1a74e310dd
simoneliskases <simon.eliskases@outlook.de>
2020-12-23 17:37:42 +0100

    Added timer script

--- Assets/MyGame/Scripts/Collectables.cs
+++ Assets/MyGame/Scripts/Collectables.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Colleactables : MonoBehaviour
+public class Collectables : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
---
+++ Assets/MyGame/Scripts/Timer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI timer;
+    public float startTime = 60;
+
+    private float time;
+
+    private void Start()
+    {
+        time = startTime;
+    }
+
+    private void Update()
+    {
+        time -= Time.deltaTime;
+
+        if(time > 0)
+        {
+            DisplayTime(time);
+        }
+        else
+        {
+            EndGame();
+        }
+    }
+
+    private void DisplayTime(float _timeToDisplay)
+    {
+        _timeToDisplay += 1;
+
+        float _minutes = Mathf.FloorToInt(_timeToDisplay / 60);
+        float _seconds = Mathf.FloorToInt(_timeToDisplay % 60);
+
+        timer.text = string.Format("{0:00}:{1:00}", _minutes, _seconds);
+    }
+
+    private void EndGame()
+    {
+        //something
+    }
+}
